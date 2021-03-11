7c1e7969eb3fc062b8221404d2b3f1605526e1cf
Yash Sharma <ys909@snu.edu.in>
2021-03-11 12:11:37 +0530

    added basic crud

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import './css/add.css'
 import add from './assets/add.png'
-import { Query, Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
+import { useMutation } from '@apollo/react-hooks';
 
 const ADD_COLOR = gql`
-    mutation Color_Mutation($hex: String!, $type: String!) {
+    mutation Add_Color_Mutation($hex: String!, $type: String!) {
         insert_colors(objects: {hex: $hex, type: $type}) {
         returning {
             hex
@@ -18,22 +18,29 @@ const ADD_COLOR = gql`
 
 function Add() {
 
+    const [addColor] = useMutation(ADD_COLOR);
+    
     function generateRandomColor(){
         var randomHex = "#"+Math.floor(Math.random()*16777215).toString(16).toString();
         var typeArray = ["Background","Neutral","Primary","Secondary"]
         var type = typeArray[[Math.floor(Math.random() * typeArray.length)]];
-
+        return {
+            variables:{
+                hex: randomHex,
+                type: type
+            }
+        };
     }
 
-    // function handleAdd(event){
-    //     event.preventDefault();
-
-    // }
+    function clickHandler(event){
+        event.preventDefault();
+        addColor(generateRandomColor())
+    }
 
     return (
         <div className="new-color">
             <h2>Add New</h2>
-            <div className="add" >
+            <div className="add" onClick={clickHandler}>
                 <img src={add}></img>
             </div>
             <p>*Adds a random color to the pallete</p>
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './css/main.css'
 import '../App.css';
 import './css/color.css'
 import Add from './Add';
-import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
 import { useSubscription } from '@apollo/react-hooks';
+import { useMutation } from '@apollo/react-hooks';
+import del from './assets/delete.png';
+import HashLoader from "react-spinners/HashLoader";
 
 const COLOR_SUBSCRIPTION = gql`
     subscription Color_Subscription {
@@ -17,7 +19,32 @@ const COLOR_SUBSCRIPTION = gql`
     }
 `
 
+const TYPE_UPDATE = gql`
+    mutation Type_Update($uid: uuid!, $type: String!) {
+        update_colors(where: {uid: {_eq: $uid}}, _set: {type: $type}) {
+            returning {
+                type
+                uid
+                hex
+            }
+        }
+    }
+`
+
+const DELETE_COLOR = gql`
+    mutation MyMutation($uid: uuid!) {
+        delete_colors_by_pk(uid: $uid) {
+            uid
+        }
+    }
+`
+
 function Main() {
+
+    const { data, error, loading } = useSubscription(COLOR_SUBSCRIPTION);
+    const [updateType] = useMutation(TYPE_UPDATE);
+    const [deleteColor] = useMutation(DELETE_COLOR);
+    const [deleteLoader, setLoading] = useState(() => { return false })
 
     function hexToRgb(hex) {
         var bigint = parseInt(hex.substring(1), 16);
@@ -37,12 +64,32 @@ function Main() {
         }
     }
 
-    const { data, error, loading } = useSubscription(COLOR_SUBSCRIPTION);
+    function handleData(event,uid) {
+        if (event.key === "Enter") {
+            updateType({ variables: { uid: uid, type: event.target.value } })
+            event.target.blur();
+        }
+    }
+
+    function clickHandler(uid) {
+        setLoading(true)
+        console.log(deleteLoader)
+        deleteColor({ variables: { uid: uid } }).then(res => {
+            setLoading(false)
+            console.log(deleteLoader)
+        })
+    }
+
     if (loading) {
-        return <p>Loading...</p>
+        return <div className="center">
+            <HashLoader size={70} color='#74b1a1'></HashLoader>
+        </div>
     }
     if (error) {
-        return <p>Error!</p>
+        return <div className="center">
+            <h1>Aww Snap!<br></br> Something went wrong</h1>
+            <p>{error}</p>
+        </div>
     }
 
     return (
@@ -55,7 +102,10 @@ function Main() {
                             <div key={uid} className="card">
                                 <div className="card_info">
                                     <p>{elementId(index)}</p>
-                                    <p>Type: {type}</p>
+                                    <div className="label">
+                                        <input id={`input-${uid}`} defaultValue={type} placeholder="Enter Label" onKeyPress={(event)=>handleData(event,uid)}></input>
+                                    </div>
+                                    <img src={del} className="delete-color" onClick={() => clickHandler(uid)}></img>
                                 </div>
                                 <div>
                                     <div style={{ backgroundColor: hex }} className="showcase-color">
